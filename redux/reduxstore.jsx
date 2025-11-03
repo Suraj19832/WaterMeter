d2cb601ad3a817596b151959a198e737dcd2e26d
suraj <suraj.desuntechnology@gmail.com>
2025-11-03 23:38:27 +0530

    push

--- redux/reduxstore.jsx
+++ redux/reduxstore.jsx
@@ -2,7 +2,6 @@
 
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/Authslice"; // Import your authSlice
-// import userReducer from './slices/userSlice'; // Import your userSlice or other slices
 import uniqueReducer from "./slices/UniqueSlice";
 import meterReducer from "./slices/MeterSlice";
 import billingReducer from "./slices/BillingSlice";
